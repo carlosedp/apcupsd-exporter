4be6e0b3e0d1b3a93767fba36a654a6de2737a58
Carlos de Paula <me@carlosedp.com>
2021-01-06 18:35:19 -0300

    Change to a target based collector with params

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -52,6 +53,7 @@ var statusList = []string{
 	"online",
 	"boost",
 	"trim",
+	"trim online",
 	"onbatt",
 	"overload",
 	"lowbatt",
@@ -164,52 +166,6 @@ var (
 		append(labels, "lasttransfer", "timetransfertobattery", "timetransferfrombattery"),
 	)
 )
-
-func main() {
-
-	// TODO: Register a port for listening here: https://github.com/prometheus/prometheus/wiki/Default-port-allocations
-	addr := flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-	upsAddr := flag.String("ups-address", "localhost:3551", "The address of the acupsd daemon to query: hostname:port")
-	flag.Parse()
-
-	log.Printf("Connection to UPS at: %s", *upsAddr)
-	log.Printf("Metric listener at: %s", *addr)
-
-	prometheus.MustRegister(status)
-	prometheus.MustRegister(statusNumeric)
-	prometheus.MustRegister(nominalPower)
-	prometheus.MustRegister(batteryChargePercent)
-	prometheus.MustRegister(timeOnBattery)
-	prometheus.MustRegister(timeLeft)
-	prometheus.MustRegister(cumTimeOnBattery)
-	prometheus.MustRegister(loadPercent)
-	prometheus.MustRegister(batteryVoltage)
-	prometheus.MustRegister(lineVoltage)
-	prometheus.MustRegister(nomBatteryVoltage)
-	prometheus.MustRegister(nomInputVoltage)
-	prometheus.MustRegister(collectSeconds)
-	prometheus.MustRegister(numTransfers)
-
-	go func() {
-		c := time.Tick(10 * time.Second)
-		for range c {
-			if err := collectUPSData(upsAddr); err != nil {
-				log.Printf("Error collecting UPS data: %+v", err)
-			}
-		}
-
-	}()
-
-	http.Handle("/metrics", promhttp.HandlerFor(
-		prometheus.DefaultGatherer,
-		promhttp.HandlerOpts{
-			// Opt into OpenMetrics to support exemplars.
-			EnableOpenMetrics: true,
-		},
-	))
-	log.Fatal(http.ListenAndServe(*addr, nil))
-
-}
 
 func collectUPSData(upsAddr *string) error {
 
@@ -364,8 +320,7 @@ func parseUnits(v string) (float64, error) {
 }
 
 func retrieveData(hostPort string) (map[string]string, error) {
-
-	conn, err := net.Dial("tcp", hostPort)
+	conn, err := net.DialTimeout("tcp", hostPort, 10*time.Second)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to connect to remote port: %+v", err)
 	}
@@ -413,4 +368,105 @@ func retrieveData(hostPort string) (map[string]string, error) {
 	}
 
 	return upsData, nil
+}
+
+func init() {
+	prometheus.MustRegister(status)
+	prometheus.MustRegister(statusNumeric)
+	prometheus.MustRegister(nominalPower)
+	prometheus.MustRegister(batteryChargePercent)
+	prometheus.MustRegister(timeOnBattery)
+	prometheus.MustRegister(timeLeft)
+	prometheus.MustRegister(cumTimeOnBattery)
+	prometheus.MustRegister(loadPercent)
+	prometheus.MustRegister(batteryVoltage)
+	prometheus.MustRegister(lineVoltage)
+	prometheus.MustRegister(nomBatteryVoltage)
+	prometheus.MustRegister(nomInputVoltage)
+	prometheus.MustRegister(collectSeconds)
+	prometheus.MustRegister(numTransfers)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+
+	target := query.Get("target")
+	if len(query["target"]) != 1 || target == "" {
+		http.Error(w, "'target' parameter must be specified once", 400)
+		return
+	}
+
+	port := query.Get("port")
+	if len(query["port"]) != 1 || port == "" {
+		http.Error(w, "'port' parameter must be specified", 400)
+		return
+	}
+
+	upsAddr := target + ":" + port
+	// upsAddr := flag.String("ups-address", "localhost:3551", "The address of the acupsd daemon to query: hostname:port")
+	flag.Parse()
+
+	log.Printf("Connection to UPS at: %s", upsAddr)
+
+	if err := collectUPSData(&upsAddr); err != nil {
+		log.Printf("Error collecting UPS data: %+v", err)
+	}
+
+	start := time.Now()
+	// Delegate http serving to Prometheus client library, which will call collector.Collect.
+	h := promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{
+		// Opt into OpenMetrics to support exemplars.
+		EnableOpenMetrics: true,
+	})
+	h.ServeHTTP(w, r)
+	duration := time.Since(start).Seconds()
+	log.Printf("Finished scrape in %f duration_seconds", duration)
+
+}
+
+func main() {
+	// TODO: Register a port for listening here: https://github.com/prometheus/prometheus/wiki/Default-port-allocations
+	addr := flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+	flag.Parse()
+	log.Printf("Metric listener at: %s", *addr)
+
+	http.Handle("/metrics", promhttp.Handler()) // Normal metrics endpoint for SNMP exporter itself.
+	// Endpoint to do SNMP scrapes.
+	http.HandleFunc("/apcupsd", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+            <head>
+            <title>apcupsd Exporter</title>
+            <style>
+            label{
+            display:inline-block;
+            width:75px;
+            }
+            form label {
+            margin: 10px;
+            }
+            form input {
+            margin: 10px;
+            }
+            </style>
+            </head>
+            <body>
+            <h1>apcupsd Exporter</h1>
+            <form action="/apcupsd">
+            <label>Target:</label> <input type="text" name="target" placeholder="X.X.X.X" value="1.2.3.4"><br>
+            <label>Port:</label> <input type="text" name="port" placeholder="3551" value="3551"><br>
+            <input type="submit" value="Submit">
+            </form>
+            </body>
+            </html>`))
+	})
+
+	// log.Printf(logger).Log("msg", "Listening on address", "address", *listenAddress)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		// level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
+		os.Exit(1)
+	}
 }
